c26385a716607c201a1ca52d5f5471f9fc9bb313
Mercury <>
2023-01-26 14:09:58 +0200

    Added FileSelectorHandler.cs
    * Creates files based on a filePath

---
+++ Assets/Presentation/FileSelector/Scripts/FileSelectorHandler.cs
@@ -0,0 +1,46 @@
+using System;
+using Core.Extensions;
+using Presentation.Components.Buttons.Scripts;
+using UnityEngine;
+
+namespace Presentation.FileSelector.Scripts {
+    public class FileSelectorHandler : MonoBehaviour {
+        [SerializeField] GameObject _actionButton;
+
+        [SerializeField] Transform _files;
+
+        string _dataPath;
+
+        public Action<string> OnFileSelected { get; set; }
+
+        void Awake() {
+            _dataPath = "Assets/Data";
+        }
+
+        void Start() {
+            PopulateFiles();
+        }
+
+        void PopulateFiles() {
+            var files = FileExtensions.GetAllFiles(_dataPath);
+
+            foreach (var file in files) {
+                if (!FileExtensions.CheckIfValidFileExtension(file, ".csv"))
+                    return;
+
+                var action = Instantiate(_actionButton, _files);
+
+                var button = action.GetComponent<FileActionHandler>();
+                if (button is null)
+                    return;
+
+                button.FilePath = file;
+                button.OnFileSelected += FileSelected;
+            }
+        }
+
+        void FileSelected(string filePath) {
+            OnFileSelected?.Invoke(filePath);
+        }
+    }
+}
