820808072c783227cc5c1433993055e43f02bddb
Mercury <>
2023-01-27 15:04:32 +0200

    Added LabelFactoryProvider.cs
    Moves label factory to a monobehaviour

---
+++ Assets/Presentation/Factories/LabelFactoryProvider.cs
@@ -0,0 +1,40 @@
+using System;
+using Core.Factory;
+using Presentation.Components.Label.Scripts;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Presentation.Factories {
+    public class LabelFactoryProvider : MonoBehaviour {
+        [SerializeField] AssetReference _reference;
+
+        FactoryBase<LabelInitializer> _factory;
+
+        public Action<LabelInitializer> OnLabelCreated { get; set; }
+
+        void Awake() {
+            _factory = new LabelFactory(_reference.AssetGUID);
+        }
+
+        void OnEnable() {
+            _factory.OnCreationDone += CreationDone;
+        }
+
+        void OnDisable() {
+            _factory.OnCreationDone -= CreationDone;
+        }
+
+        void CreationDone(LabelInitializer label) {
+            if (label is null) {
+                Debug.LogError("Failed to get Label prefab.");
+                return;
+            }
+
+            OnLabelCreated?.Invoke(label);
+        }
+
+        public void RequestLabel() {
+            _factory.RequestCreation();
+        }
+    }
+}
