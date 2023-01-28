4e82ebcd6fc5945af0ee575ec911565ef8b13942
Mercury <>
2023-01-28 18:37:26 +0200

    Added StartingScreen.cs and it's prefab
    A welcome screen to start using the tool

---
+++ Assets/Presentation/Components/Menu/Scripts/StartingScreen.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.MixedReality.Toolkit;
+using UnityEngine;
+
+namespace Presentation.Components.Menu.Scripts {
+    public class StartingScreen : MonoBehaviour {
+        public StatefulInteractable _startButton;
+
+        public Action OnStartClicked { get; set; }
+
+        void OnEnable() {
+            _startButton?.OnClicked.AddListener(StartVisualization);
+        }
+
+        void OnDisable() {
+            _startButton?.OnClicked.RemoveListener(StartVisualization);
+        }
+
+        void StartVisualization() {
+            OnStartClicked?.Invoke();
+        }
+    }
+}
