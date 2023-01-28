5fd9094f2a944d8fbda23da4194899a4d2aeb2be
Mercury <>
2023-01-28 22:38:28 +0200

    Updated DataVisualizer.cs
    * Created a recursive function to create groups
    * Connects data labels with a line

--- Assets/Presentation/Visualizer/DataVisualizer.cs
+++ Assets/Presentation/Visualizer/DataVisualizer.cs
@@ -1,33 +1,99 @@
-using Core.DataProcessor;
-using Core.FileReader;
-using Infrastructure.DataProcessor;
-using Infrastructure.FileReader;
-using Presentation.FileSelector.Scripts;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Extensions;
+using Presentation.Components.Label.Scripts;
+using Presentation.Components.Line.Scripts;
+using Presentation.Factories;
+using Presentation.LayerOrganizer;
 using UnityEngine;
 
 namespace Presentation.Visualizer {
-	public class DataVisualizer : MonoBehaviour {
+    public class DataVisualizer : MonoBehaviour {
+        [SerializeField] LayerOrganizerManager _layerOrganizer;
 
-		[SerializeField] FileSelectorHandler _setup;
+        [SerializeField] Transform _dataParent;
 
-		IFileReader<string[], string> _fileReader;
-		IDataProcessor<string, string[,]> _dataProcessor;
+        [SerializeField] LabelFactoryProvider _labelFactory;
 
-		void Awake() {
-			_fileReader = new CSVReader();
-			_dataProcessor = new CSVDataProcessor(_fileReader);
-		}
+        [Header("Prefabs")] [SerializeField] GameObject _horizontalGroup;
+        [SerializeField] LineDrawer _linePrefab;
 
-		void OnEnable() {
-			_setup.OnFileSelected += FileSelected;
-		}
+        HashSet<string> _entriesByOrder;
+        LabelInitializer _labelGameObject;
 
-		void OnDisable() {
-			_setup.OnFileSelected -= FileSelected;
-		}
+        void Start() {
+            _labelFactory.OnLabelCreated += LabelCreated;
+            _labelFactory.RequestLabel();
+        }
 
-		void FileSelected(string filePath) {
-			var data = _dataProcessor.ProcessData(filePath);
-		}
-	}
+        void OnEnable() {
+            _layerOrganizer.OnOrderConfirmed += OrderConfirmed;
+        }
+
+        void OnDisable() {
+            _layerOrganizer.OnOrderConfirmed -= OrderConfirmed;
+        }
+
+        void OrderConfirmed(HashSet<int> order, string[,] data) {
+            if (order.Count <= 0)
+                return;
+            if (data is null)
+                return;
+
+            var orderArr = order.ToArray();
+
+            var orderedEntries = MatrixExtensions.GetColumnUniqueEntries(data, orderArr[0]);
+
+            var layers = new Transform[orderArr.Length];
+            for (var i = 0; i < orderArr.Length; i++) {
+                layers[i] = Instantiate(_horizontalGroup, _dataParent).transform;
+            }
+
+            foreach (var entry in orderedEntries)
+                GenerateGroup(data, orderArr, 0, entry, null, layers);
+        }
+
+        void GenerateGroup(string[,] data, int[] order, int k, string entryData, Transform upperLabel,
+            Transform[] layers) {
+            if (k >= order.Length)
+                return;
+
+            if (string.IsNullOrWhiteSpace(entryData))
+                return;
+
+            if (k == 0) {
+                var label = CreateLabel(entryData, layers[k]);
+                GenerateGroup(data, order, 1, entryData, label.transform, layers);
+
+                return;
+            }
+
+            var dataLabels = MatrixExtensions.GroupDataByColumnKey(data, entryData, order, k);
+
+            CreateLabelWithLine(data, order, k, upperLabel, layers, dataLabels);
+        }
+
+        void CreateLabelWithLine(string[,] data, int[] order, int k, Transform upperLabel, Transform[] layers,
+            HashSet<string> dataLabels) {
+            foreach (var value in dataLabels) {
+                var line = Instantiate(_linePrefab, layers[k]);
+                var label = CreateLabel(value, layers[k]);
+                line.SetLineBetweenTwoPoints(upperLabel, label.transform);
+
+                GenerateGroup(data, order, k + 1, value, label.transform, layers);
+            }
+        }
+
+        Transform CreateLabel(string title, Transform parent) {
+            var label = Instantiate(_labelGameObject, parent);
+            label.Title = title;
+
+            return label.transform;
+        }
+
+        void LabelCreated(LabelInitializer label) {
+            _labelGameObject = label;
+            _labelFactory.OnLabelCreated -= LabelCreated;
+        }
+    }
 }
